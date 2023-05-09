3d025f7754da9e4e9c1746eb223adad02e0c8347
Vooidy <gustavo.os2004@gmail.com>
2023-05-09 19:05:33 -0300

    inicio do projeto

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+    <html lang="pt">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title> 
+            <script src="styles/style.js"></script>
+            <link rel="stylesheet" href="styles/reset.css">
+            <link rel="stylesheet" href="styles/style.css">
+        </head>
+        <body>
+            <div class="conteudo">
+                <div class="pessoa">
+                    <div class="imagem">
+                        <img src="https://www.locaweb.com.br/blog/wp-content/uploads/2008/12/publico-alvo-e-persona.jpg">
+                    </div>
+                    <div class="nome">
+                        Miguelzinho delas
+                    </div>
+                    <div class="descricao">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan efficitur risus a efficitur. Proin congue tempus vehicula. Aliquam tempor nisl ut faucibus consectetur
+                    </div>
+                    <div class="nascimento">
+                        09/05/2001
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
